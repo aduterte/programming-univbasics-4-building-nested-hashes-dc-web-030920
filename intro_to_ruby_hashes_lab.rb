d081c7aa182cd80a9702d3ac60b626f5f44c84e5
Alessandro Duterte <alexduterte@gmail.com>
2020-01-28 03:48:18 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,7 @@ def monopoly_with_third_tier
   hash = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => [1,2,3,4]
+      :rent_in_dollars => {:1 => {},:2 => {}, :3 => {}, :4 =>{}}
 
     }}
   hash
